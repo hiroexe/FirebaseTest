4bb818558ab312886ace2fe7f913f8ab40806c6b
HiroExe <78053102+hiroexe@users.noreply.github.com>
2022-10-10 15:19:11 +0200

    Add files via upload

---
+++ FirebaseTest/FirebaseTest/FirebaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTestApp.swift
+//  FirebaseTest
+//
+//  Created by stefano.carella on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
